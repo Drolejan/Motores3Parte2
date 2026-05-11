c8e909313cf18cadc859602c0a778c6855d0cf0d
Alejandro Medina <drolejan@gmail.com>
2026-05-10 22:22:03 -0600

    Update GameManager.cs

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,16 +1,53 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static GameManager Instance;
+
+    [SerializeField] private int dinero = 0;
+    [SerializeField] private List<ItemData> inventario = new List<ItemData>();
+
+    public int Dinero => dinero;
+    public List<ItemData> Inventario => inventario;
+
+    public event Action<int> OnDineroChanged;
+    public event Action<ItemData> OnItemAdded;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddItem(ItemData item)
     {
-        
+        switch (item.tipo)
+        {
+            case ItemType.Moneda:
+                dinero += item.valor;
+                OnDineroChanged?.Invoke(dinero);
+                break;
+
+            case ItemType.Collecionable:
+            case ItemType.Importante:
+                inventario.Add(item);
+                OnItemAdded?.Invoke(item);
+                break;
+
+            case ItemType.Vida:
+                Debug.Log("Curar vida: " + item.valor);
+                break;
+
+            case ItemType.Stamina:
+                Debug.Log("Recuperar stamina: " + item.valor);
+                break;
+        }
     }
 }
