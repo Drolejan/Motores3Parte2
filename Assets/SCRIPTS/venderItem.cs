ef74acb1c9da92b1a21a9744eec7fdea02a8494c
Alejandro Medina <drolejan@gmail.com>
2026-05-25 08:31:31 -0600

    Update venderItem.cs

--- Assets/SCRIPTS/venderItem.cs
+++ Assets/SCRIPTS/venderItem.cs
@@ -20,3 +20,57 @@ public class venderItem : MonoBehaviour
         
     }
 }
+
+/*
+using TMPro;
+using UnityEngine;
+
+public class SlotInventarioUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI nombreItemText;
+
+    private ItemData item;
+
+    public void Configurar(ItemData nuevoItem)
+    {
+        item = nuevoItem;
+        nombreItemText.text = item.nombreItem;
+    }
+
+    public void UsarItem()
+    {
+        switch (item.tipo)
+        {
+            case ItemType.Vida:
+                Debug.Log("Usaste item de vida: " + item.valor);
+                GameManager.Instance.RemoveItem(item);
+                break;
+
+            case ItemType.Stamina:
+                Debug.Log("Usaste item de stamina: " + item.valor);
+                GameManager.Instance.RemoveItem(item);
+                break;
+
+            case ItemType.Importante:
+                Debug.Log("Este item es importante y no se consume");
+                break;
+
+            case ItemType.Collecionable:
+                Debug.Log("Este item es coleccionable");
+                break;
+        }
+    }
+
+    public void VenderItem()
+    {
+        if (item.tipo == ItemType.Importante)
+        {
+            Debug.Log("No puedes vender un item importante");
+            return;
+        }
+
+        GameManager.Instance.AddMoney(item.valor);
+        GameManager.Instance.RemoveItem(item);
+    }
+}
+*/
